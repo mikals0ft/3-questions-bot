8183a4f318841b2854b456bfe6e0c2f9b038f28f
Risa Pesavento <risa.pesavento@discordapp.com>
2023-05-23 00:21:43 +0000

    Start using interactions library

--- Bot.py
+++ Bot.py
@@ -1,24 +1,62 @@
-import discord
-from Functions.ThreeQuestions.threequestions import ThreeQuestions
-from discord.ext import commands
 import os
+import os.path
+import random
 
-import settings
-
-intents = discord.Intents.all()
-
-intents.members = True
-
-cogs: list = ["Functions.ThreeQuestions.threequestions"]
-
-
-bot = commands.Bot(command_prefix=settings.Prefix, help_command=None, intents=intents)
+import interactions
+import openai
+import asyncio
+from interactions import autodefer
 
 
-@bot.event
-async def on_ready():
-    print("Bot is ready!")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(settings.BotStatus))
-    await bot.add_cog(ThreeQuestions(bot))
+# Set up OpenAI
+MODEL = 'gpt-3-5-turbo-discord'
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-bot.run(os.environ['BOT_TOKEN'])
+bot = interactions.Client(token='MTExMDMwOTQ1NTQ5NDY1NjAwMQ.GwpVfV.AnZN9ANJiO5CJrYzDJjYlLig5LK5AA_5azD_zw')
+
+question_bank = open(os.path.dirname(__file__) + '/questions.txt')
+questions = question_bank.readlines()
+
+prompt = {
+    'role': 'user',
+    'content': 'Generate 1 question for friends in a Discord server who already know each other well. The questions are funny and quirky. The questions should be answerable in 1-3 open-ended words. They are outputted in numbered list.',
+}
+openai_response = openai.ChatCompletion.create(model=MODEL, temperature=0, messages=[prompt])
+openai_result = openai_response.choices[0].message.content
+
+@bot.command(
+    name='hello_command', description='Just prints "hello"!', scope=1110296888953032837,
+)
+async def hello(ctx: interactions.CommandContext):
+    await ctx.send('Hello World')
+
+
+@bot.command(
+    name='questionbank',
+    description='Prints up to 10 questions from the bank',
+    scope=1110296888953032837,
+    options=[
+        interactions.Option(
+            name='num_questions',
+            description='number of questions to output',
+            type=interactions.OptionType.INTEGER,
+            required=True,
+        ),
+    ],
+)
+async def questionbank(ctx: interactions.CommandContext, num_questions: int):
+    if num_questions > 10:
+        await ctx.send('Cannot output more than 10 questions from question bank')
+    else:
+        top_n_questions = '\n'.join(random.sample(questions, num_questions))
+        await ctx.send(top_n_questions)
+
+
+@bot.command(
+    name='openai_test', description='Calls OpenAi to generate questions', scope=1110296888953032837,
+)
+@autodefer()
+async def openai_test(ctx: interactions.CommandContext):
+    await ctx.send(openai_result)
+
+bot.start()
